2a4c5e2c2053c4972af5e7205dbaaf7a3dfaca2f
arthurlanz <arthurlanznaster@gmail.com>
2026-09-09 09:26:37 -0300

    feat: aprimora a função save_formset para gerenciar itens de pedido

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -18,3 +18,23 @@ class PedidoAdmin(admin.ModelAdmin):
     search_fields = ['usuario__email', 'usuario__name', 'codigo_retirada']
     readonly_fields = ['codigo_retirada', 'data']
     inlines = [ItemPedidoInline]
+
+    def save_formset(self, request, form, formset, change):
+        """Preenche automaticamente o preço do produto ao criar itens."""
+
+        instances = formset.save(commit=False)
+
+        # Remove itens marcados para exclusão.
+        for instance in formset.deleted_objects:
+            instance.delete()
+
+        # Salva os itens novos/alterados.
+        for instance in instances:
+            if isinstance(instance, ItemPedido):
+                # O preço é definido automaticamente apenas para itens novos.
+                if instance.pk is None and instance.produto_id:
+                    instance.preco_unitario = instance.produto.preco_atual
+
+            instance.save()
+
+        formset.save_m2m()
